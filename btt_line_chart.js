da4ecb0e724df22cc04fb8f6b5001da2a47418f7
Prabha Arivalagan <prabhakaran.mails@gmail.com>
2024-08-05 12:11:09 -0500

    Update btt_line_chart.js

--- btt_line_chart.js
+++ btt_line_chart.js
@@ -1,11 +1,7 @@
 looker.plugins.visualizations.add({
-  // Id and Label are legacy properties that no longer have any function besides documenting
-  // what the visualization used to have. The properties are now set via the manifest
-  // form within the admin/visualizations page of Looker
   id: "btt_line_chart",
   label: "LCP Comparison Chart",
   options: {
-    // Add any configurable options here
     color_this_period: {
       type: "string",
       label: "This Period Color",
@@ -18,9 +14,7 @@ looker.plugins.visualizations.add({
     }
   },
   
-  // Set up the initial state of the visualization
   create: function(element, config) {
-    // Insert a <style> tag with some styles we'll use later.
     element.innerHTML = `
       <style>
         .lcp-chart {
@@ -33,110 +27,67 @@ looker.plugins.visualizations.add({
     this.chart = null;
   },
 
-  // Render in response to the data or settings changing
   updateAsync: function(data, element, config, queryResponse, details, done) {
-    // Clear any errors from previous updates
     this.clearErrors();
 
-    // Throw some errors and exit if the shape of the data isn't what we expect
-    if (queryResponse.fields.dimensions.length == 0) {
-      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
+    console.log("Data:", data);
+    console.log("Query Response:", queryResponse);
+
+    if (data.length === 0) {
+      this.addError({title: "No Data", message: "The query returned no results."});
       return;
     }
-    if (queryResponse.fields.measures.length == 0) {
-      this.addError({title: "No Measures", message: "This chart requires measures."});
-      return;
-    }
 
-    // Extract the data from the Looker query response
-    const extractData = (data, queryResponse) => {
-      const dateField = queryResponse.fields.dimensions[0].name;
-      const lcpField = queryResponse.fields.measures[0].name;
-      const pivotField = queryResponse.pivots[0].key;
+    try {
+      const dateField = queryResponse.fields.dimensions.find(d => d.type === 'date' || d.type === 'datetime')?.name;
+      const measureField = queryResponse.fields.measures[0]?.name;
 
-      return data.reduce((acc, row) => {
-        const date = row[dateField].value;
-        const pivotValue = row[pivotField][lcpField].value;
+      if (!dateField) {
+        this.addError({title: "No Date Dimension", message: "This chart requires a date dimension."});
+        return;
+      }
+      if (!measureField) {
+        this.addError({title: "No Measure", message: "This chart requires a measure."});
+        return;
+      }
 
-        if (!acc[date]) {
-          acc[date] = {};
-        }
-        acc[date][pivotField] = pivotValue;
-        return acc;
-      }, {});
-    };
+      const chartData = data.map(row => ({
+        date: row[dateField].value,
+        value: row[measureField].value
+      }));
 
-    const extractedData = extractData(data, queryResponse);
+      const chartContainer = element.querySelector('.lcp-chart');
 
-    // Transform the data for Highcharts
-    const transformData = (data) => {
-      const thisPeriodData = [];
-      const previousPeriodData = [];
+      if (this.chart) {
+        this.chart.destroy();
+      }
 
-      Object.entries(data).forEach(([date, values]) => {
-        if (values['This Period']) {
-          thisPeriodData.push([new Date(date).getTime(), values['This Period']]);
-        }
-        if (values['Previous Period']) {
-          previousPeriodData.push([new Date(date).getTime(), values['Previous Period']]);
-        }
+      this.chart = Highcharts.chart(chartContainer, {
+        title: {
+          text: 'Data Over Time'
+        },
+        xAxis: {
+          type: 'datetime',
+          title: {
+            text: 'Date'
+          }
+        },
+        yAxis: {
+          title: {
+            text: queryResponse.fields.measures[0].label_short || 'Value'
+          }
+        },
+        series: [{
+          name: measureField,
+          data: chartData.map(d => [new Date(d.date).getTime(), parseFloat(d.value)]),
+          color: config.color_this_period
+        }]
       });
 
-      return [thisPeriodData, previousPeriodData];
-    };
-
-    const [thisPeriodData, previousPeriodData] = transformData(extractedData);
-
-    // Get the chart container
-    const chartContainer = element.querySelector('.lcp-chart');
-
-    // Destroy existing chart if it exists
-    if (this.chart) {
-      this.chart.destroy();
+      done();
+    } catch (error) {
+      console.error("Error in visualization:", error);
+      this.addError({title: "Error", message: "An error occurred while rendering the visualization. Check the console for details."});
     }
-
-    // Create the chart
-    this.chart = Highcharts.chart(chartContainer, {
-      title: {
-        text: 'LCP Over Time - Current vs Previous Period'
-      },
-      xAxis: {
-        type: 'datetime',
-        title: {
-          text: 'Date'
-        }
-      },
-      yAxis: {
-        title: {
-          text: 'LCP (seconds)'
-        }
-      },
-      tooltip: {
-        shared: true,
-        formatter: function() {
-          return this.points.reduce((s, point) => {
-            return s + '<br/>' + point.series.name + ': <b>' + point.y.toFixed(2) + '</b>';
-          }, '<b>' + Highcharts.dateFormat('%b %e, %Y', this.x) + '</b>');
-        }
-      },
-      series: [{
-        name: 'This Period LCP',
-        data: thisPeriodData,
-        color: config.color_this_period,
-        marker: {
-          symbol: 'circle'
-        }
-      }, {
-        name: 'Previous Period LCP',
-        data: previousPeriodData,
-        color: config.color_previous_period,
-        marker: {
-          symbol: 'square'
-        }
-      }]
-    });
-
-    // We are done rendering! Let Looker know.
-    done();
   }
 });
